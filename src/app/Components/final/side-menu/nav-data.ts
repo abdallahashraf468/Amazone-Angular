4580b9de89e203ba88b8e75db6a9414c9de86703
Muhammad Omar <m.elnatory@gmail.com>
2023-11-07 12:22:23 +0200

    some change Muhammad

--- src/app/Components/final/side-menu/nav-data.ts
+++ src/app/Components/final/side-menu/nav-data.ts
@@ -14,26 +14,31 @@ export const navbarData: InavbarData[] = [
       {
         routeLink: 'products/level1.1',
         label: 'Level 1.1',
-        items: [
-          {
-            routeLink: 'products/level2.1',
-            label: 'Level 2.1',
-          },
-          {
-            routeLink: 'products/level2.2',
-            label: 'Level 2.2',
-            items: [
-              {
-                routeLink: 'products/level3.1',
-                label: 'Level 3.1',
-              },
-              {
-                routeLink: 'products/level3.2',
-                label: 'Level 3.2',
-              }
-            ]
-          }
-        ],
+        expanded: false,
+        // items: [
+        //   {
+        //     routeLink: 'products/level2.1',
+        //     label: 'Level 2.1',
+        //     expanded: false,
+        //   },
+        //   {
+        //     routeLink: 'products/level2.2',
+        //     label: 'Level 2.2',
+        //     expanded: false,
+        //     items: [
+        //       {
+        //         routeLink: 'products/level3.1',
+        //         label: 'Level 3.1',
+        //         expanded: false,
+        //       },
+        //       {
+        //         routeLink: 'products/level3.2',
+        //         label: 'Level 3.2',
+        //         expanded: false,
+        //       }
+        //     ]
+        //   }
+        // ],
       },
       {
         routeLink: 'products/level1.2',
