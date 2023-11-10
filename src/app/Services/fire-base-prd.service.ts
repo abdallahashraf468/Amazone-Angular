d93d557da46b3de2b17458e6af3e7412cee9e00e
Fady Shenoda Masoud <fadyshenoda0o0@gmail.com>
2023-11-10 17:00:18 +0200

    before agular material

--- src/app/Services/fire-base-prd.service.ts
+++ src/app/Services/fire-base-prd.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Firestore, addDoc, collection, collectionData, deleteDoc, doc, getDoc, getDocs } from '@angular/fire/firestore';
+import { Firestore, addDoc, collection, collectionData, deleteDoc, doc, getDoc, getDocs, updateDoc } from '@angular/fire/firestore';
 import { IfireBseProduct } from '../Models/ifire-base-prd';
 
 @Injectable({
@@ -15,5 +15,30 @@ export class FirebasePrdService {
 
   addProduct(product:IfireBseProduct){
     addDoc(collection(this.fsObject,'products'),product);
+  }
+  updateProduct(product:IfireBseProduct){
+    const productObject = { ...product };
+    let prdRef = doc(this.fsObject,'products',product.id);
+    updateDoc(prdRef,productObject);
+  };
+  getBrands(){
+    let brands = collection(this.fsObject,'brands');
+    return collectionData(brands,{idField:'id'});
+  }
+  getCtaegories(){
+    let categories = collection(this.fsObject,'categories');
+    return collectionData(categories,{idField:'id'});
+  }
+  getSubCategories(){
+    let subCategories = collection(this.fsObject,'sub-ategories');
+    return collectionData(subCategories,{idField:'id'});
+  }
+  getOrders(){
+    let order=collection(this.fsObject,'orders');
+    return collectionData(order,{idField:'id'});
+  }
+  getUsers(){
+    let users=collection(this.fsObject,'users');
+    return collectionData(users,{idField:'id'});
   }
 }
